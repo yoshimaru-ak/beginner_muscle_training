23b466e82e201bae6e2b90c5ce797a902c12921e
yoshimaru-ak <59866256+yoshimaru-ak@users.noreply.github.com>
2020-03-05 20:21:13 +0900

    Update seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,17 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+
+
+
+
+item_list = [
+  {name: "胸筋", image: "https://www.pakutaso.com/shared/img/thumb/BO15103044151030444987_TP_V.jpg"},
+  {name: "背中", image: "https://www.pakutaso.com/shared/img/thumb/BO151030454998_TP_V.jpg"},
+  {name: "腕", image: "https://pakutaso.cdn.rabify.me/shared/img/thumb/BO151030495155.jpg?d=500"},
+  {name: "脚", image: "https://pakutaso.cdn.rabify.me/shared/img/thumb/BO151030305553.jpg?d=500"},
+  {name: "肩", image: "https://pakutaso.cdn.rabify.me/shared/img/thumb/BO15103059151030595160.jpg?d=500"},
+  {name: "腹筋", image: "https://pakutaso.cdn.rabify.me/shared/img/thumb/BO151030535008.jpg?d=500"},
+]
+
+item_list.each do |item|
+  Item.create(name: item[:name], image: item[:image])
+end
